675ce0ad4208d3f8d6933622b47c8b1791995eee
David Bouzas Kabundi <davidbouzas02@gmail.com>
2023-11-13 15:38:52 +0000

    Ejercicio 1

---
+++ Unidad1/Unidad1.java
@@ -0,0 +1,19 @@
+package Unidad1;
+public class Unidad1 {
+    public static void main(String[] args) {
+        ejercicio1();
+}
+public static void ejercicio1(){
+    int A = 5, B = 2, C = 5;
+    System.out.println(A = B);
+    System.out.println(C = A);
+    System.out.println(B = (A + B + C) / 2);
+    System.out.println(A = A + C);
+    System.out.println(C = B - A);
+    System.out.println(C = C - A);
+    System.out.println(A = A * B);
+    System.out.println(A = A + 3);
+    System.out.println(A = A % B);
+    System.out.println(C = C + A);
+}
+}
