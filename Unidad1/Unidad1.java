5acbe779d0ac3243b8329c8fb1cd978ffe47e293
David Bouzas Kabundi <davidbouzas02@gmail.com>
2023-11-13 18:28:18 +0000

    Ejercicio 5

--- Unidad1/Unidad1.java
+++ Unidad1/Unidad1.java
@@ -62,6 +62,21 @@ public static void ejercicio2(){
 
 public static void ejercicio5(){
     //Evalúa las siguientes expresiones.
+    System.out.println("#1 = " + 24 % 5);
+    System.out.println("#2 = " + 7/2 + 2.5);
+    System.out.println("#3 = " + 10.8/2 + 2);
+    System.out.println("#4 = " + (4 + 6) * 3 + 2 * (5 - 1));
+    System.out.println("#5 = " + 5/2 + 17 % 3);
+    System.out.println("#6 = " + ((7 >= 5) || (27 != 8)));
+    System.out.println("#7 = " + ((45 <= 7) || !(5 >= 7)));
+    System.out.println("#8 = " + (27 % 4 + 15/4));
+    System.out.println("#9 = " + (37 / 4 * 4 - 2));
+    System.out.println("#10 = " + ((25>20) && !(7 <= 2)));
+    System.out.println("#11 = " + (('H'<'J') && ('9'!='7')));
+    System.out.println("#12 = " + ((25>20) && (13>5)));
+    System.out.println("#13 = " + (((10+4)<(15-3))||((2*5+1)>(14-2*2))));
+    System.out.println("#14 = " + ((4*2 <= 8) || (2*2 < 5) && (4 > 3 + 1)));
+    System.out.println("#15 = " + ((10<=2*5)&&(3<4)|| !(8>7) && (3*2 <= 4*2-1)));
 }
 
 public static void ejercicio6(){
