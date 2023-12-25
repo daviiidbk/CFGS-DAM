4a6438c0bf2942ad01b8cb3b4c4fbba3cb381470
David Bouzas Kabundi <133178123+daviiidbk@users.noreply.github.com>
2023-12-25 17:47:29 +0100

    Creación de Unidad 3 + Ejercicio 1

---
+++ Unidad3/Unidad3.java
@@ -0,0 +1,18 @@
+package Unidad3;
+import Java.util.scanner;
+
+public class Unidad3 {
+    public static void main(String[] args){
+        ejercicio1();
+    }
+    public static void ejercicio1(){
+        /* Elabora un programa que muestra por 
+         * pantalla los 20 primeros números naturales.
+         */
+        int num = 0;
+        while(num <= 20){
+            num++;
+            System.out.println(num);
+        }
+    }
+}
